34eae1ec0e7f6587c340fcbbdf6b966ed09eec36
gagaswin <gagaswahyuin@gmail.com>
2025-02-10 10:44:38 +0700

    initalize git

---
+++ src/main/java/com/bank/gagas/GagasApplication.java
@@ -0,0 +1,13 @@
+package com.bank.gagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GagasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/gagas/GagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.gagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
